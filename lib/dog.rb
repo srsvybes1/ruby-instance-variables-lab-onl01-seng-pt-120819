738706844fcea3437a1aa5726a843a9e238ca85f
Cody Toney-Griffith <cody.toney@gmail.com>
2020-01-06 13:53:51 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class dog 
+  def name = (dog_name)
+    @this_dog_name = dog_name 
+  end 
+  
+  def name 
+    @this_dog_name
+  end 
+end 
+
+lassie = dog.new 
+lassie.name = "Lassie"
+puts lassie.name
